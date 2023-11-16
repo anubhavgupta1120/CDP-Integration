5511412df68582c8f4ffd3898f9b8592987e66cf
anubhavgupta1120 <anubhavgp11@gmail.com>
2023-11-16 19:31:44 +0530

    New Files as well as Readme is also added

--- src/test/java/TestFolder/BlockedNetworkRequest.java
+++ src/test/java/TestFolder/BlockedNetworkRequest.java
@@ -1,13 +1,19 @@
 package TestFolder;
 
 import org.testng.annotations.Test;
+
+import com.google.common.collect.ImmutableList;
+
 import org.testng.annotations.BeforeTest;
 
 import java.time.Duration;
+import java.util.Optional;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chromium.ChromiumDriver;
 import org.openqa.selenium.devtools.DevTools;
+import org.openqa.selenium.devtools.v119.network.Network;
 import org.testng.annotations.AfterTest;
 
 public class BlockedNetworkRequest {
@@ -16,6 +22,11 @@ public class BlockedNetworkRequest {
 
 	@Test
 	public void BlockingNetworkReqTest() {
+		driver.get("https://rahulshettyacademy.com/angularAppdemo/");
+		driver.findElement(By.cssSelector(".btn-success")).click();
+		driver.findElement(By.xpath("//a[text() = 'Selenium']")).click();
+		driver.findElement(By.cssSelector(".add-to-cart")).click();
+		System.out.println(driver.findElement(By.className("sp")).getText());
 	}
 
 	@BeforeTest
@@ -23,8 +34,9 @@ public class BlockedNetworkRequest {
 		driver = new ChromeDriver();
 		devTools = driver.getDevTools();
 		devTools.createSession();
-
+		BlockRequest();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.manage().window().maximize();
 	}
 
 	@AfterTest
@@ -32,5 +44,9 @@ public class BlockedNetworkRequest {
 		devTools.disconnectSession();
 		driver.quit();
 	}
-
+	
+	public void BlockRequest() {
+		devTools.send(Network.enable(Optional.empty(), Optional.empty(), Optional.empty()));
+		devTools.send(Network.setBlockedURLs(ImmutableList.of("*.jpg", "*.css")));
+	}
 }
